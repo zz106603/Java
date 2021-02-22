931ad001229545a1c119592d27d811d2ecb92af7
YUNHWAN <629jyh7@naver.com>
2021-02-22 14:59:10 +0900

    소스 추가

---
+++ src/ch06/exam14/Example.java
@@ -0,0 +1,13 @@
+package ch06.exam14;
+
+public class Example {
+
+	public static void main(String[] args) {
+		
+		System.out.println(Korean.nation);
+		
+		Korean k1 = new Korean("000000");
+
+	}
+
+}
---
+++ src/ch06/exam14/Korean.java
@@ -0,0 +1,12 @@
+package ch06.exam14;
+
+public class Korean {
+
+	 final static String nation = "ÇÑ±¹";
+	 final String ssn;
+	 
+	 Korean(String ssn){
+		 this.ssn = ssn;
+	 }
+}
+
